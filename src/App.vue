eaf395a98549375528a1e89b6ff402cc1b946f56
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2021-12-03 11:56:31 +0400

    finished one

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>{{title}}</h1>
+<teleport to=".modals" v-if="showModal">
+  <Modal theme="sale" @close="toggleModal">
+    <template v-slot:links> 
+      <a href="#">sign up now</a>
+      <a href="#">more info</a>
+    </template>
+    <h1>Ninja Givaway</h1>
+    <p>Grab your ninja swag for half price!</p>
+  </Modal>
+</teleport>
+  <teleport to=".modals" v-if="showModaltwo">
+  <Modal theme="" @close="toggleModaltwo">
+    <template v-slot:links> 
+      <a href="#">sign up now</a>
+      <a href="#">more info</a>
+    </template>
+    <h1>Beka modal Givaway</h1>
+    <p>Grab your modal for free!</p>
+  </Modal>
+</teleport>
+
+
+<p>welcome</p>
+<button @click.alt="toggleModal">open modal (alt) </button>
+<button @click.alt="toggleModaltwo">open modaltwo (alt) </button>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {Modal},
+  
+  data(){
+    return{
+    title: 'my first but not exactly Vue App',
+    showModal: false,
+    showModaltwo: false,
+    }
+  },
+  methods: {
+    toggleModal(){
+      this.showModal = !this.showModal
+    },
+    toggleModaltwo(){
+      this.showModaltwo = !this.showModaltwo
+    },
+    handleclick(){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
   }
 }
 </script>
 
 <style>
-#app {
+#app, .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -23,4 +68,10 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+
 </style>
